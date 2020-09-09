06a3d30b48672dfaceaa6ee4248acf86dfca6630
casjay <casjay@yahoo.com>
2020-09-09 14:58:50 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+mkdir -p /srv/docker/registry-web && chmod -Rf 777 /srv/docker/registry-web
+
+docker run --name registry-web \
+-d --restart=always \
+-e ENV_DOCKER_REGISTRY_HOST=$HOSTNAME \
+-e ENV_DOCKER_REGISTRY_PORT=5000 \
+-e ENV_REGISTRY_PROXY_FQDN=$HOSTNAME \
+-e ENV_REGISTRY_PROXY_PORT=443 \
+-e ENV_DEFAULT_REPOSITORIES_PER_PAGE=50 \
+-e ENV_MODE_BROWSE_ONLY=false \
+-e ENV_DEFAULT_TAGS_PER_PAGE=20 \
+-e ENV_DOCKER_REGISTRY_USE_SSL=1 \
+-e ENV_USE_SSL=1 \
+-v /srv/docker/registry-web:/var/lib/registry \
+-v /etc/ssl/CA/CasjaysDev/certs/localhost.crt:/etc/apache2/server.crt:ro \
+-v /etc/ssl/CA/CasjaysDev/private/localhost.key:/etc/apache2/server.key:ro \
+-p 7080:80 \
+-p 7081:443 \
+konradkleine/docker-registry-frontend:v2
